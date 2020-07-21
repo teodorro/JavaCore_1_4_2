a562395b2590ac9b07b6badf284ba430a53cd719
teodorro <edem.teo@gmail.com>
2020-07-21 14:24:43 +0300

    .

--- src/com/company/TaskSave.java
+++ src/com/company/TaskSave.java
@@ -10,24 +10,22 @@ import java.util.zip.ZipOutputStream;
 public class TaskSave {
     private final String PATH = "D://Games//savegames";
     private final String ZIP_PATH = "D://Games//savegames//gameprogress.zip";
-    List<GameProgress> progresses;
-    List<String> filePaths = new ArrayList<>();
 
     public void start() {
-        createGameProgresses();
-        saveGameProgresses();
-        zipGameProgresses();
-        deleteNotZipped();
+        List<GameProgress> progresses = createGameProgresses();
+        List<String> filePaths = saveGameProgresses(progresses);
+        zipGameProgresses(filePaths);
+        deleteNotZipped(filePaths);
     }
 
-    private void deleteNotZipped() {
+    private void deleteNotZipped(List<String> filePaths) {
         for (int i = 0; i < filePaths.size(); i++) {
             File file = new File(filePaths.get(i));
             file.delete();
         }
     }
 
-    private void zipGameProgresses() {
+    private void zipGameProgresses(List<String> filePaths) {
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(ZIP_PATH))) {
             for (int i = 0; i < filePaths.size(); i++) {
                 try (FileInputStream fis = new FileInputStream(filePaths.get(i))) {
@@ -50,7 +48,8 @@ public class TaskSave {
         }
     }
 
-    private void saveGameProgresses() {
+    private List<String> saveGameProgresses(List<GameProgress> progresses) {
+        List<String> filePaths = new ArrayList<>();
         for (int i = 0; i < progresses.size(); i++) {
             String path = PATH + "//save" + i + ".dat";
             try (FileOutputStream fos = new FileOutputStream(path);
@@ -63,10 +62,11 @@ public class TaskSave {
                 System.out.println(e.getMessage());
             }
         }
+        return filePaths;
     }
 
-    private void createGameProgresses() {
-        progresses = Arrays.asList(
+    private List<GameProgress> createGameProgresses() {
+        return Arrays.asList(
                 new GameProgress(51, 52, 53, 54),
                 new GameProgress(61, 62, 63, 64),
                 new GameProgress(71, 72, 73, 74)
